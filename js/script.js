71ac231452e9dc9adab42cd4bab51974fcffc924
SamNwaoshai <snwaoshai@student.sdccd.edu>
2024-03-20 17:27:53 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', () => {
+    const $ = (id) =>
+    document.getElementById(id);
+    let form = $('empForm');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        console.log("ID:" + $('id').value);
+        console.log("Name:" + $('name').value);
+        console.log("Extension:" + $('ext').value);
+        console.log("Email:" + $('email').value);
+        console.log("Department:" + $('department').value)
+    }
+    )
+}
+)
